c84ef69b89b05fc2aaf0c301f7e37327c90e2d5a
Alex Marushka <marushkaalex@gmail.com>
2017-02-26 02:15:34 +0300

    Get expected value

--- lab1.py
+++ lab1.py
@@ -3,15 +3,18 @@
 import csv
 
 def main():
+	for i in range(-20, 31, 5):
+		print(i)
 	with open('data.txt') as csvfile:
-		map = [0 for i in range(-20, 36, 5)]
+		t_list = [0 for i in range(-20, 31, 5)]
 		for row in csvfile:
 			for i in range(12):
 				str = row[11 + i * 6:17 + i * 6]
 				index = get_index(float(str))
-				map[index] += 1
+				t_list[index] += 1
 
-		print(map)
+		print(t_list)
+		get_expected_value(t_list)
 
 def get_index(temparature):
 	index = 0
@@ -21,6 +24,15 @@ def get_index(temparature):
 		else:
 			index += 1
 	return index
+
+def get_expected_value(t_list):
+	count = 0
+	sum = 0
+	for index, value in enumerate(t_list):
+		count += value;
+		sum += (-20 + index * 5) * value
+
+	return sum / count
 
 if __name__ == '__main__':
 	main()
